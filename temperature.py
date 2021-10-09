17e5734ae458244e99eb3711b8691cd30823b6f3
Elizabeth <easeverance@gmail.com>
2021-10-09 13:44:56 -0500

    Adding PyPoll_Challenge.py file

--- temperature.py
+++ temperature.py
@@ -5,4 +5,4 @@ else:
     print("Open the windows")
 import datetime as dt
 now=dt.datetime.now()
-print("The time right nw is ", now)
+print("The time right now is ", now)
--- while_and_for_loops.py
+++ while_and_for_loops.py
@@ -3,6 +3,4 @@ while x<=5:
     print(x)
     x=x+2
 count=7
-while count<=8:
-    print("Hello World")
-    x=x+1
+
